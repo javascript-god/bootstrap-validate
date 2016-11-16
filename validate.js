85297a995446d28c4084c5c057689f7e52c1342f
Satnam Singh <Satnam@satnams-mbp.home>
2016-11-16 22:43:07 +0000

    Updating code

--- validate.js
+++ validate.js
@@ -1,23 +1,40 @@
 
+// TODO: Refactor selector
+function getInputFields(form){
+	return form.querySelectorAll('input:not(.form-check-input)');
+}
 
-var inputFields = document.querySelectorAll('input:not(.form-check-input)');
+function getElement (selector, context = document) {
+	return context.querySelector(selector);
+}
+
+function addEvents (el, evt, handler) {
+	el.addEventListener(evt, handler);
+}
 		
-function validateForm() {
+function validateForm(selector) {
 
-	for(var counter = 0, valid = 0; counter < inputFields.length; counter++) {
-		if(inputFields[counter].value.length >= 6) {
-			valid++;
+	function onformSubmit(e) {
+		e.preventDefault();
+		for(var counter = 0, valid = 0; counter < inputFields.length; counter++) {
+			if(inputFields[counter].value.length >= 6) {
+				valid++;
+			}
+		}
+
+		if(valid === inputFields.length) {
+			alert('valid!');
+			this.submit();
+		}
+		else {
+			console.log('invalid!'); 
 		}
 	}
 
-	if(valid === inputFields.length) {
-		console.log('valid!')
-	}
-	else {
-		console.log('invalid!');
-	}
-
+	var form = getElement(selector);	
+	var inputFields = getInputFields(form);
+	console.log(getElement('#exampleInputPassword1', form));
+	addEvents(form, 'submit', onformSubmit);
 }
 
-var buttonSubmit = document.querySelector('.btn');
-buttonSubmit.addEventListener("click", validateForm);
+validateForm('#login');
